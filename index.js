c63f4670d39050333238151e33ada9e4a9b8cdac
Code-daniil <danya.krut2017@gmail.com>
2025-11-22 15:03:42 +0300

    Первый загруз сайта

--- index.js
+++ index.js
@@ -2,3 +2,31 @@ window.addEventListener('load', () => {
     document.querySelector('.preloader').classList.add('hide');
     document.body.classList.remove('loading');
 });
+// Перебираем все секции
+document.querySelectorAll('.screen').forEach(screen => {
+  const food = screen.querySelector('.food');
+  const checkbox = screen.querySelector('.toggle');
+  const overlay = screen.querySelector('.close-overlay');
+  const productName = screen.querySelector('.product-name');
+
+  // Следим за сменой состояния checkbox
+  checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      food.classList.add('blurred');
+      productName.classList.add('visible');
+      overlay.classList.add('active');
+    } else {
+      food.classList.remove('blurred');
+      productName.classList.remove('visible');
+      overlay.classList.remove('active');
+    }
+  });
+
+  // Клик по overlay снимает чек и скрывает блюр
+  overlay.addEventListener('click', () => {
+    checkbox.checked = true;
+    food.classList.remove('blurred');
+    productName.classList.remove('visible');
+    overlay.classList.remove('active');
+  });
+});
